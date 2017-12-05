96b869da95053e5c6513a52002bb4c3015e5fbf2
Mario <zhangbiaoFT@163.com>
2017-12-05 15:01:25 +0800

    post 请求提交

--- demo/05_post.js
+++ demo/05_post.js
@@ -2,6 +2,7 @@
  * 配合form.html页面模拟一个post请求，从而理解nodejs post请求的参数获取
  */
 var http = require("http");
+var querystring = require('querystring');
 
 // 创建服务器
 var server = http.createServer(function (req, res) {
@@ -14,9 +15,13 @@ var server = http.createServer(function (req, res) {
             console.log(chunk); // <Buffer ……>
         });
         req.addListener('end', function () {
-            console.log(alldata.toString())
+            // console.log(alldata.toString())
+            var datastring = alldata.toString();
             res.writeHead(200,{'Content-Type':'text/html'});
             res.end('SUCCESS');
+            // 将datastring 转为一个对象
+            var dataObj = querystring.parse(datastring);
+            console.log(dataObj); // { name: 'Fin', sex: '男', hobby: [ '睡觉', '撸码', '打怪' ] }
         })
     }
 })
