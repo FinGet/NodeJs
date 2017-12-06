637bbd81fa7c06b43ed68bc5fb1c89172c77780a
Mario <zhangbiaoFT@163.com>
2017-12-06 11:05:45 +0800

    next

--- express/08_next.js
+++ express/08_next.js
@@ -4,7 +4,7 @@ var app = express();
 app.get("/",function(req,res,next){
     console.log(1);
     res.send('success1');
-    next(); // 不加这个语句,下面的事件不会触发
+    next(); // 不加这个语句,下面的事件不会触发  但是这里send()之后，下面就不能再send()否则会报错
 });
 
 app.get("/",function(req,res){
